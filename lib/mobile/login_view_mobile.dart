43b5837e3244ef1df1724266db9ba14357304984
Muskan Poddar <poddarmuskan888@gmail.com>
2025-08-21 22:05:47 +0530

    Modified login view mobile file

--- lib/mobile/login_view_mobile.dart
+++ lib/mobile/login_view_mobile.dart
@@ -1,11 +1,27 @@
+import 'package:Budget_App/view_model.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class LoginViewMobile extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    TextEditingController _emailField = useTextEditingController();
+    TextEditingController _passwordField = useTextEditingController();
+    final viewModelProvider = ref.watch(viewModel);
+    final double deviceHeight = MediaQuery.of(context).size.height;
     return SafeArea(
-      child: Scaffold(),
+      child: Scaffold(
+        body: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(height: deviceHeight / 5.5),
+              Image.asset("assets/logo.png", fit: BoxFit.contain, width: 210.0),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
