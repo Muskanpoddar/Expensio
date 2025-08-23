30046286f027489ec490a6008a1447f0298c0cd8
Muskan Poddar <poddarmuskan888@gmail.com>
2025-08-23 23:56:33 +0530

    Modified expense_view mobile file

--- lib/mobile/expense_view_mobile.dart
+++ lib/mobile/expense_view_mobile.dart
@@ -1,13 +1,63 @@
+import 'package:Budget_App/view_model.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-
-bool isLoading = true;
 
 class ExpenseViewMobile extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final viewModelProvider = ref.watch(viewModel);
+    double deviceWidth = MediaQuery.of(context).size.width;
+
+    int totalExpense = 0;
+    int totalIncome = 0;
+
+    void calculate() {
+      for (int i = 0; i < viewModelProvider.expensesAmount.length; i++) {
+        totalExpense =
+            totalExpense + int.parse(viewModelProvider.expensesAmount[i]);
+      }
+
+      for (int i = 0; i < viewModelProvider.incomesAmount.length; i++) {
+        totalIncome =
+            totalIncome + int.parse(viewModelProvider.incomesAmount[i]);
+      }
+    }
+
+    calculate();
+    int budgetLeft = totalIncome - totalExpense;
+
     return SafeArea(
-      child: Scaffold(),
+      child: Scaffold(
+        drawer: Drawer(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              DrawerHeader(
+                padding: EdgeInsets.only(bottom: 20.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    border: Border.all(width: 1.0, color: Colors.black),
+                  ),
+                  child: CircleAvatar(
+                    radius: 180.0,
+                    backgroundColor: Colors.white,
+                    child: Image(
+                      height: 100.0,
+                      image: AssetImage('assets/logo.png'),
+                      filterQuality: FilterQuality.high,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        appBar: AppBar(
+          iconTheme: IconThemeData(color: Colors.white, size: 30.0),
+          backgroundColor: Colors.black,
+        ),
+      ),
     );
   }
 }
